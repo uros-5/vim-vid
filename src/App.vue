5b460d9c2ab3dcf0db0c50e67c7144a3389966a9
uros <uros.mrkobrada99@gmail.com>
2023-06-27 20:38:57 +0200

    generate new project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+<div class="bg-gradient-to-b from-slate-300 from-20% dark:from-10% dark:from-slate-800 to-slate-500 dark:to-slate-600 w-full h-screen bg-no-repeat">
+  <RouterView v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </RouterView>  
+</div>
+
+</template>
