a875e180387ad64eb8c8750272a235e24e97f605
uros <uros.mrkobrada99@gmail.com>
2023-06-29 10:49:50 +0200

    added main header

---
+++ src/components/icons/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex justify-center py-5 gap-12">
+    <VimVidLogo />
+    <QuestionMark width="10%" class="" />
+  </header>
+</template>
+
+<script setup lang="ts">
+import VimVidLogo from './VimVidLogo.vue';
+import QuestionMark from "./QuestionMark.vue";
+
+
+</script>
+
+<style></style>
---
+++ src/components/icons/VimVidLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link to="/">
+      <InlineSvg src="src/assets/mainlogo.svg" />
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import InlineSvg from "vue-inline-svg"
+
+</script>
+
+<style></style>
