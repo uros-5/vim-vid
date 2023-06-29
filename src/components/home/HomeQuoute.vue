0090946f98cdea9ddad0e58f72bdbeda5d199f45
uros <uros.mrkobrada99@gmail.com>
2023-06-29 10:50:31 +0200

    index page finished

---
+++ src/components/home/HomeQuoute.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3 class="text-5xl p-10 my-16 selection:bg-red-200 selection:text-white">
+    Edit videos in <b class="text-slate-800">Vim</b> style.
+  </h3>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+h3 {
+    font-family: 'Poppins', sans-serif;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import HomeFile from '@/components/home/HomeFile.vue';
+import HomeQuoute from '@/components/home/HomeQuoute.vue';
+
 </script>
 
 <template>
-  <main class="">
+  <main class="flex flex-col items-center">
+    <HomeQuoute />
+    <HomeFile />
   </main>
 </template>
+
