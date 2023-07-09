9637fc07226249235ad10764fea5b2de4ecf62ec
uros <uros.mrkobrada99@gmail.com>
2023-07-10 00:15:02 +0200

    removed temp class from CurrentCommand

--- src/components/editor/CurrentCommand.vue
+++ src/components/editor/CurrentCommand.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="col-start-2 row-start-4 lg:col-start-5 lg:row-start-1 text-center py-2 text-xl lg:text-2xl lg:flex lg:justify-center lg:items-center temp"
+    class="col-start-2 row-start-5 lg:col-start-5 lg:row-start-3 text-center py-2 text-xl lg:text-2xl lg:flex lg:justify-center lg:items-center"
   >
     <transition name="current-command">
       <p class="text-slate-950 dark:text-slate-200">Pressed: <b>com</b></p>
