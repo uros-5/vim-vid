99a35af0b3d5af5464b7fdf551aa0ae1d2363e0b
uros <uros.mrkobrada99@gmail.com>
2023-07-10 18:33:07 +0200

    yank ffmpeg commands is added

--- src/stores/vimvid.ts
+++ src/stores/vimvid.ts
@@ -1,9 +1,12 @@
-import { defineStore } from 'pinia'
+import { defineStore, type _ActionsTree } from 'pinia'
 import keys from 'ctrl-keys'
-import { Clips, Clip, type EditorAction, type EditorBuffer, Markers, emptyClip, type StorageSave } from './vimvid-types'
+import { Clips, Clip, EditorAction, type EditorBuffer, Markers, emptyClip, type StorageSave } from './vimvid-types'
 import { ref } from 'vue'
 import router from '@/router'
 import { z } from 'zod'
+import startSound from '@/assets/sounds/start.ogg'
+import stopSound from '@/assets/sounds/stop.ogg'
+import deleteSound from '@/assets/sounds/delete.ogg'
 
 export const vimvid = defineStore('vimvid', () => {
   const video = ref(null as HTMLVideoElement | null)
@@ -24,11 +27,11 @@ export const vimvid = defineStore('vimvid', () => {
   const recordingClip = ref(emptyClip());
   const ls = readLocalStorage();
   const darkMode = ref(false);
+  const volume = ref(1);
   lastTime.value = ls.lastTime;
   clips.value = ls.clips;
   markers.value = ls.markers;
   darkMode.value = ls.darkMode;
-  console.log(ls)
 
   const handler = keys()
   handler
@@ -43,7 +46,26 @@ export const vimvid = defineStore('vimvid', () => {
         video.value.paused ? video.value.play() : video.value.pause()
       }
     })
-    .add('i', () => { })
+    .add(',', () => {
+      if (volume.value <= 1) {
+        volume.value += 0.1;
+      }
+    })
+    .add('.', () => {
+      if (volume.value <= 1) {
+        volume.value -= 0.1;
+      }
+      else { volume.value = 1; }
+    })
+    .add('m', () => {
+      const last = clips.value.at(-1);
+      if (last) {
+        if (!markers.value.find(item => item.id == last.id)) {
+          markers.value.push({ id: last.id })
+          saveLocalStorage('markers', markers.value);
+        }
+      }
+    })
     .add('o', () => {
       if (video.value) {
         switch (isRecording.value) {
@@ -51,6 +73,7 @@ export const vimvid = defineStore('vimvid', () => {
             {
               isRecording.value = true;
               recordingClip.value.start = video.value.currentTime;
+              sound(EditorAction.StartRecording);
               break;
             }
           case true:
@@ -77,6 +100,7 @@ export const vimvid = defineStore('vimvid', () => {
                 elem.scrollLeft = elem.scrollWidth;
               }
               saveLocalStorage('clips', clips.value);
+              sound(EditorAction.StopRecording);
               break;
             }
         }
@@ -94,7 +118,13 @@ export const vimvid = defineStore('vimvid', () => {
         video.value.playbackRate = currentSpeed.value
       }
     })
-    .add('x', () => { })
+    .add('x', () => {
+      if (clips.value.length > 0) {
+        clips.value.pop();
+        saveLocalStorage('clips', clips.value);
+        sound(EditorAction.DeleteLastClip);
+      }
+    })
     .add('shift+h', () => {
       router.push('/')
     })
@@ -111,7 +141,27 @@ export const vimvid = defineStore('vimvid', () => {
     .add('ctrl+c', () => { })
     .add('ctrl+m', () => { })
     .add('0', () => { })
-    .add('shift+c', () => { })
+    .add('shift+y', async () => {
+      if (clips.value.length > 0) {
+        let command = `#!/usr/bin/sh \nmkdir vimvid\nmkdir vimvid/clips vimvid/merged\n\nvidsrc=""\n`;
+        let mergeCommand = "ffmpeg ";
+        let mergeAudio = "";
+        let mergeVideo = "";
+        let filterArray = "";
+        command += "\n#all clips\n";
+        clips.value.forEach((item,i)=> {
+          command += `ffmpeg -ss ${item.start} -i "$vidsrc" -c:v copy -c:a copy -t ${item.end - item.start} "vimvid/clips/video-${item.id}.mp4"\n`;
+          mergeCommand += `-i "vimvid/clips/video-${item.id}.mp4" `;
+          filterArray += `[${i}:a] `
+        })
+        command += "\n#merging\n";
+        mergeAudio = `${mergeCommand} -filter_complex "${filterArray}concat=n=${clips.value.length}:v=0:a=1 [a]" -map "[a]" "vimvid/merged/audio.aac"`;
+        mergeVideo = `${mergeCommand} -filter_complex "${filterArray.replaceAll('a', 'v')}concat=n=${clips.value.length}:v=1:a=0 [v]" -map "[v]" "vimvid/merged/video.mp4"`;
+        command += `${mergeAudio}\n${mergeVideo}\n`
+        await navigator.clipboard.writeText(command);
+      }
+    })
+    .add('ctrl+l', (e) => { e?.preventDefault(); console.log(`oooh here l`) })
     .add('ctrl+o', (e) => {
       e?.preventDefault()
       fileElem.value?.click()
@@ -119,6 +169,22 @@ export const vimvid = defineStore('vimvid', () => {
 
   window.addEventListener('keydown', handler.handle)
 
+
+  function sound(action: EditorAction) {
+    let audio;
+    switch (action) {
+      case EditorAction.StartRecording:
+        { audio = startSound; break; }
+      case EditorAction.StopRecording:
+        { audio = stopSound; break; }
+      case EditorAction.DeleteLastClip:
+        { audio = deleteSound; break; }
+      default: return;
+    }
+    const a = new Audio(audio);
+    a.volume = volume.value
+    a.play();
+  }
   return {
     video: video,
     videoBlob,
@@ -169,7 +235,6 @@ function readLocalStorage() {
   let markers = Markers.safeParse(JSON.parse(localStorage.getItem('markers')!));
   let lastTime = z.number().safeParse(JSON.parse(localStorage.getItem('lastTime')!));
   let obj = { darkMode: false, clips: [] as Clips, markers: [] as Markers, lastTime: 0 };
-  console.log(darkMode)
   if (darkMode.success) obj.darkMode = darkMode.data;
   if (clips.success) obj.clips = clips.data
   if (markers.success) obj.markers = markers.data;
@@ -180,3 +245,6 @@ function readLocalStorage() {
 function saveLocalStorage(key: StorageSave, value: any) {
   localStorage.setItem(key, JSON.stringify(value))
 }
+
+
+
