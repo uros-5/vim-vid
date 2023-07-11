a0270c479aa585263997cc3656e727ed83ce5ffd
uros <uros.mrkobrada99@gmail.com>
2023-07-12 00:11:34 +0200

    using Help components

---
+++ src/views/HelpView.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- -->
+  <section class="m-auto w-4/5">
+    <div class="flex flex-col justify-center gap-6 px-3 my-6">
+      <HelpIntroduction />
+      <HelpTable />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import HelpIntroduction from '@/components/help/HelpIntroduction.vue'
+import HelpTable from '@/components/help/HelpTable.vue'
+</script>
+
+<style scoped>
+p {
+  font-family: 'Forum', cursive;
+}
+
+.main-page {
+  line-height: 2em;
+}
+</style>
