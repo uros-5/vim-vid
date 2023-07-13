d653eae2b582868948d8c2021fa0b8eca964ec57
uros <uros.mrkobrada99@gmail.com>
2023-07-13 22:28:45 +0200

    main logo is now imported directly into component

--- src/components/icons/VimVidLogo.vue
+++ src/components/icons/VimVidLogo.vue
@@ -1,11 +1,12 @@
 <template>
   <router-link to="/">
-    <InlineSvg src="src/assets/mainlogo.svg" />
+    <InlineSvg :src="mainLogo" />
   </router-link>
 </template>
 
 <script setup lang="ts">
 import InlineSvg from 'vue-inline-svg'
+import mainLogo from '@/assets/mainlogo.svg'
 </script>
 
 <style></style>
