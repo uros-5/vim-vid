1e2219a99afda23a2d512dde45697729390ea2f1
Uros <59397844+uros-5@users.noreply.github.com>
2023-07-14 08:26:37 +0200

    fixed padding for span

--- src/components/help/HelpIntroduction.vue
+++ src/components/help/HelpIntroduction.vue
@@ -8,13 +8,13 @@
     class="text-xl lg:text-3xl text-slate-950 dark:text-slate-200 lg:leading-[2.1em] lg:text-justify selection:bg-green-700"
   >
     Current keybindings depends on the
-    <span @mouseover="contextHover" class="font-bold p-2 bg-slate-400 dark:bg-slate-800"
+    <span @mouseover="contextHover" class="font-bold p-px lg:p-2 bg-slate-400 dark:bg-slate-800"
       >context</span
     >
     in which they are used. For example if you are in
     <RouterLink class="underline" to="/editor">editor</RouterLink> page and selected context is your
     clips(<i>element below main video player</i>) then your
-    <code class="font-bold p-2 bg-slate-400 dark:bg-slate-800">h or l</code> command will select one
+    <code class="font-bold p-px lg:p-2 bg-slate-400 dark:bg-slate-800">h or l</code> command will select one
     of your clips. Selected clips can be previewed on second video player. If same commands are
     executed in main context, main video will go backwards/forwards by one step(<i
       >default value is 3 seconds</i
